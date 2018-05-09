e5bdbdfb7778882aba1b283a0c1bdd9707bb1b12
buttermatt <38929744+buttermatt@users.noreply.github.com>
2018-05-09 16:21:45 +1200

    updated

--- secondSection.cpp
+++ secondSection.cpp
@@ -1,12 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
+//initing the adc reading values
+int scan_front;
+int scan_right1;
+int scan_right2;
+int scan_left1;
+int scan_left2;
+
+// input numbers to change on prop code
+int f = 0;
+int r1 = 1;
+int r2 = 2;
+int l1 = 3;
+int l2 = 4;
+
+int distFromWall = 600;
+int sleepAmount = 10000;
+
+
+int mazeSection()
+{
+
+
+	scan_front = read_analog(f); 
+	scan_right1 = read_analog(r1);
+	scan_right2 = read_analog(r2);
+	scan_left1 = read_analog(l1);
+	scan_left2 = read_analog(l2);
 
 // Tests sensing for wall infront
+	if (scan_front > 100 )
+	{
 // When finds one -> sensors for walls on either side
 // Turns that direction
+	}
+
 // Tests for pink tape 
+	if (/*test for pink tape below*/)
+	{
 //    Waits till door is closed
 //    Then waits till door is open
 //    Wait afew more secs
 //    Then continues on its way
+	}
+
+
+	sleep1(0,sleepAmount);
+	return 0;
+}
