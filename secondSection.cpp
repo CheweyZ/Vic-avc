9f2eb4226891493fadee281474163f6a9b4e27ca
buttermatt <38929744+buttermatt@users.noreply.github.com>
2018-05-09 16:01:38 +1200

    Create secondSection.cpp
    for the maze area

---
+++ secondSection.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+// Tests sensing for wall infront
+// When finds one -> sensors for walls on either side
+// Turns that direction
+// Tests for pink tape 
+//    Waits till door is closed
+//    Then waits till door is open
+//    Wait afew more secs
+//    Then continues on its way
