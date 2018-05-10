9422f156507dc8265fb19ac9ac13364a79e68e75
thebanterage <thebanterage@gmail.com>
2018-05-10 23:48:33 +1200

    Add files via upload
    need to work on it some more Arthur said there was a method to condense into 5 lines of code. Will possibly explore that.

---
+++ gates.cpp
@@ -0,0 +1,12 @@
+//conecting to the gate started working on it
+//port is 1024 from arthur
+//password is Please
+//Supposedly supposed to be 5 lines of code
+char message[24]("P","l","e","a","s","e"); //I need to make sure that this method works not sure it does
+int conect_to_server (char serverAddr[15], int 1024;)
+int recieve_from_server(char message[24]) )
+int send_to_server(char message[24])
+if (send_to_server == true){
+  printf("This is supposed to work I believe\n", );
+
+}
