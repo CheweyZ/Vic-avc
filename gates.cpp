4872dab83a0ea0575f2e3c679e2a2367f3b1478a
thebanterage <thebanterage@gmail.com>
2018-05-17 13:05:07 +1200

    Update gates.cpp

--- gates.cpp
+++ gates.cpp
@@ -5,6 +5,39 @@
 //Supposedly supposed to be 5 lines of code
 //char serverAddr[15]("130.195.6.196"); pretty sure i needed this but again I'm not sure which one to do, my noobyness doesn't know
 //char serverAddr[15]("1","3","0",".","1","9","5",".","6",".","1","9","6",);
+
+
+//need to test these properly and rearrange code properly -
+/**int lmspeed = 0; 
+int rmspeed = 0;
+int scan_front = 100;
+int maxMotorSpeed = 0;
+int updateMotorSpeed() {
+	return 0;
+} */ //neccesarily dont even need here assuming this gets c
+
+
+//started working then realised that I wasnt working in atom lol.
+int walltest = 0;
+//testing if the gate is present in the code. 
+int (int walltest) { 
+	lMSpd = maxMotorSpeed;
+	rMSpd = maxMotorSpeed;
+	    updateMotorSpeed(); 
+	sleep1(1, 0); //sleep for 
+		bool walltest = false; //changes if the gate rising is detected
+		if (scan_front < 300) { 
+			if (scan_front > 400) {
+				upgate = true;
+			}
+	}
+	return 0;
+}
+if (walltest == true){
+
+
+
+
 char message[24]("P","l","e","a","s","e"); //I need to make sure that this method works not sure it does
 int conect_to_server (char serverAddr[15], int 1024;)
 int recieve_from_server(char message[24]) )
