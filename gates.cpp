5bbd2bcdc3f701b5f7354f0db3f59697df41f82c
thebanterage <thebanterage@gmail.com>
2018-05-17 13:47:35 +1200

    Update gates.cpp

--- gates.cpp
+++ gates.cpp
@@ -1,54 +1,3 @@
-//big tidy up is required. 
-//conecting to the gate started working on it
-// 130.195.6.196
-//port is 1024 from arthur
-//password is Please
-//Supposedly supposed to be 5 lines of code
-//char serverAddr[15]("130.195.6.196"); pretty sure i needed this but again I'm not sure which one to do, my noobyness doesn't know
-//char serverAddr[15]("1","3","0",".","1","9","5",".","6",".","1","9","6",);
-
-
-//need to test these properly and rearrange code properly -
-/**int lmspeed = 0; 
-int rmspeed = 0;
-int scan_front = 100;
-int maxMotorSpeed = 0;
-int updateMotorSpeed() {
-	return 0;
-} */ //neccesarily dont even need here assuming this gets c
-
-
-//started working then realised that I wasnt working in atom lol.
-//
-int walltest = 0;
-//testing if the gate is present in the code. 
-int (int walltest) { 
-	lMSpd = maxMotorSpeed;
-	rMSpd = maxMotorSpeed;
-	    updateMotorSpeed(); 
-	sleep1(1, 0); //sleep for 
-		bool walltest = false; //changes if the gate rising is detected
-		if (scan_front < 300) { 
-			if (scan_front > 400) {
-				upgate = true;
-			}
-	}
-	return 0;
-}
-if (walltest == true){
-
-
-
-
-char message[24]("P","l","e","a","s","e"); //I need to make sure that this method works not sure it does
-int conect_to_server (char serverAddr[15], int 1024;)
-int recieve_from_server(char message[24]) )
-int send_to_server(char message[24])
-if (send_to_server == true){
-  printf("This is supposed to work I believe\n", );
-
-}
-
 //conecting to the gate started working on it - having a mad brain fade today grrr. Apologies on the garbage/
 //port is 1024 from arthur
 //password is Please
@@ -92,10 +41,6 @@ if (walltest == true){
   UI.printf("JET FUEL CANT MELT STEEL BEAMS"\n", );
   //then proceed to sending message??
   
-
-//needs to test for wall infront before sending message
-//continues moving after wall goes up (also after delay)
-//changes section number
 
 //needs to test for wall infront before sending message
 //continues moving after wall goes up (also after delay)
