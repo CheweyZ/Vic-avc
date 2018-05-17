78907b9b1718e6d9b22ceedfaf62beb5ff7ead2d
thebanterage <thebanterage@gmail.com>
2018-05-17 12:30:37 +1200

    Update gates.cpp

--- gates.cpp
+++ gates.cpp
@@ -3,6 +3,8 @@
 //port is 1024 from arthur
 //password is Please
 //Supposedly supposed to be 5 lines of code
+//char serverAddr[15]("130.195.6.196"); pretty sure i needed this but again I'm not sure which one to do, my noobyness doesn't know
+//char serverAddr[15]("1","3","0",".","1","9","5",".","6",".","1","9","6",);
 char message[24]("P","l","e","a","s","e"); //I need to make sure that this method works not sure it does
 int conect_to_server (char serverAddr[15], int 1024;)
 int recieve_from_server(char message[24]) )
