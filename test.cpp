d5e151f3d507b37b032a81b90f7dd41cc825f71c
buttermatt <38929744+buttermatt@users.noreply.github.com>
2018-05-18 12:25:41 +1200

    testing the connection with gate 1

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "E101.h"
+
+char serverAddr[15]("130.195.6.196");
+char message[24]("P","l","e","a","s","e");
+int conect_to_server (serverAddr[15], 1024;);
+int recieve_from_server(message[24]);
+int send_to_server(message[24]);
+if (send_to_server == true){
+  printf("done\n", );
+}
