8c63018b2415bd79edc742b948d8e8c7e5ac9edb
buttermatt <38929744+buttermatt@users.noreply.github.com>
2018-05-18 12:36:30 +1200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,9 +3,9 @@
 #include <ctype.h>
 #include "E101.h"
 
-char serverAddr[15]("130.195.6.196");
-char message[24]("P","l","e","a","s","e");
-int conect_to_server (serverAddr[15], 1024;);
+char serverAddr[15]=("130.195.6.196");
+char message[24]={"P","l","e","a","s","e"};
+int conect_to_server(serverAddr[15], 1024;);
 int recieve_from_server(message[24]);
 int send_to_server(message[24]);
 if (send_to_server == true){
