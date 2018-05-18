d194d0e859d0e35f787684990936961a3d42440d
buttermatt <38929744+buttermatt@users.noreply.github.com>
2018-05-18 13:28:32 +1200

    updated gate test to be formatted correctly

--- test.cpp
+++ test.cpp
@@ -3,10 +3,19 @@
 #include <ctype.h>
 #include "E101.h"
 
-char[15] serverAddr=("130.195.6.196");
-char[24] message=("Please");
-char[24] password;
-int connect_to_server(serverAddr, 1024);
-int send_to_server(message);
-int recieve_from_server(password);
-int send_to_server(password);
+int main()
+{
+	init();
+	passwordGate();
+	return 0;
+}
+
+void passwordGate(){
+	char[15] serverAddr=("130.195.6.196");
+	char[24] message=("Please");
+	char[24] password;
+	int connect_to_server(serverAddr, 1024);
+	int send_to_server(message);
+	int recieve_from_server(password);
+	int send_to_server(password);
+}
