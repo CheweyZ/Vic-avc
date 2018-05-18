4712a2cf8eb866d107775ecceec5fd852a2e9d83
buttermatt <38929744+buttermatt@users.noreply.github.com>
2018-05-18 14:05:03 +1200

    fixed unneeded ints

--- test.cpp
+++ test.cpp
@@ -14,9 +14,9 @@ void passwordGate(){
 	char serverAddr[15]={'1','3','0','.','1','9','5','.','6','.','1','9','6'};
 	char message[24]={'P','l','e','a','s','e'};
 	char password[24];
-	int connect_to_server(serverAddr, 1024);
-	int send_to_server(message);
-	int recieve_from_server(password);
+	connect_to_server(serverAddr, 1024);
+	send_to_server(message);
+	recieve_from_server(password);
 	printf(password);
 	int send_to_server(password);
 }
