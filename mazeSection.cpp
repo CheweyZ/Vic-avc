92313df160eb75e1e2b686a7c08ff44bf0898fb4
buttermatt <38929744+buttermatt@users.noreply.github.com>
2018-05-23 13:25:58 +1200

    updated with print statements for testing

--- mazeSection.cpp
+++ mazeSection.cpp
@@ -8,19 +8,19 @@ int scan_front;
 int scan_right;
 int scan_left;
 
-// input pin numbers to change on prop code
-int f = 0;
-int r = 1;
-int l = 2;
+// input pin numbers
+int l = 0;
+int f = 1;
+int r = 2;
 
-// input pin numbers for motors to change on prop code
+// input pin numbers for motors
 int leftMotor = 1;
 int rightMotor = 2;
 
 int distFromWall = 600;
 int distFromSideWall = 100;
 int sleepAmount = 10000;
-int turning = 0; //(-ve(Left),0(Straight),+ve(Right))
+int mazeTurning = 0; //(-ve(Left),0(Straight),+ve(Right))
 
 int loopForceTimer = 100000;
 
@@ -28,15 +28,15 @@ int loopForceTimer = 100000;
 //functions
 ////////////////////////////////
 
-void turn(int lSpeed, int rSpeed){  //change to main codes turn
+void mazeTurn(int lSpeed, int rSpeed){  //change to main codes turn
   printf("left: %d ... ", lSpeed);
   printf("right: %d\n", rSpeed);
   //set_motor(rightMotor,lSpeed);
   //set_motor(leftMotor,rSpeed);
 }
 
-void forward(){ //goes straight
-  turn(100,100);
+void mazeForward(){ //goes straight
+  mazeTurn(100,100);
 }
 
 void turnOnSpot(int speed){
@@ -49,9 +49,9 @@ void turnOnSpot(int speed){
 void scanValueUpdate()
 {
 // updating values
-  //scan_front = read_analog(f); // front
-  //scan_right = read_analog(r); // right
-  //scan_left = read_analog(l); // left
+  scan_front = read_analog(f); // front
+  scan_right = read_analog(r); // right
+  scan_left = read_analog(l); // left
 }
 
 void testTurn(){
@@ -60,72 +60,82 @@ void testTurn(){
   {
     if (scan_right > distFromSideWall) //there seems to be no wall there
     {
-      turning = 1;
+      mazeTurning = 1;
     } 
     else if (scan_left > distFromSideWall)
     {
-      turning = -1;
+      mazeTurning = -1;
+    }
+    else
+    {
+      mazeTurning = 10;
     }
   }
   //reallign if front(/back) is too close on one side if nec
-  //change code to mayb?
-  if (scan_left > scan_right + mazeCentraliseThreshold && scan_right > noWallSenseThreshold && turning == 0)
+  if (scan_left > scan_right + mazeCentraliseThreshold && scan_right > noWallSenseThreshold && mazeTurning == 0)
   { // realign right
-    turning = -2;
+    mazeTurning = -2;
   }
-  else if (scan_right > scan_right + mazeCentraliseThreshold && scan_right > noWallSenseThreshold && turning == 0)
+  else if (scan_right > scan_left + mazeCentraliseThreshold && scan_right > noWallSenseThreshold && mazeTurning == 0)
   { // realign left
-    turning = 2;
+    mazeTurning = 2;
   }
-  /*if (scan_left > distFromWall && turning == 0) //change to test to adjust when: one side is bigger than the other, no side is giving ~0 (gap in wall) and is not turning
+  //old realign code
+  /*if (scan_left > distFromWall && mazeTurning == 0) //change to test to adjust when: one side is bigger than the other, no side is giving ~0 (gap in wall) and is not mazeTurning
   { // realign right
-    turning = -2;
+    mazeTurning = -2;
   }
-  else if (scan_right > distFromWall && turning == 0)
+  else if (scan_right > distFromWall && mazeTurning == 0)
   { // realign left
-    turning = 2;
+    mazeTurning = 2;
   }*/
 
 }
 
 void mazeMove(){
-  //turning/reallign function
-  if (turning == 1) //turning left
+  //Turning/reallign function
+  if (mazeTurning == 1) //Turning left
   {
-    turn(10,100);
+    mazeTurn(10,100);
     if (scan_front < distFromWall) //if large enough gap in front of robot
     {
-      turning == 0;
+      mazeTurning == 0;
     } 
   }
-  else if (turning == -1) //turning right
+  else if (mazeTurning == -1) //Turning right
   {
-    turn(100,10); 
+    mazeTurn(100,10); 
     if (scan_front < distFromWall) //if large enough gap in front of robot
     {
-      turning == 0;
+      mazeTurning == 0;
     }
   }
-  else if (turning == 2) //realign left
+  else if (mazeTurning == 2) //realign left
   {
-    turn(100,50);
+    mazeTurn(100,50);
     if (scan_right < distFromSideWall)
     { // if scan of each wall is roughly similar
-      turning == 0;
+      mazeTurning == 0;
     }
   }
-  else if (turning == -2) //realign right
+  else if (mazeTurning == -2) //realign right
   {
-    turn(50,100);
+    mazeTurn(50,100);
     if (scan_left < distFromSideWall)
     {
-      turning == 0;
+      mazeTurning == 0;
     }
   }
+  else if (mazeTurning == 10)
+  { // going backwards but shouldnt be needed to be called
+  	printf("error\n");
+
+  }
+  printf("front: %d left: %d right: %d turnNum: %d\n",scan_front,scan_left,scan_right,mazeTurning);
   // going straight or updated to go straight
-  if (turning = 0)
+  if (mazeTurning == 0)
   {
-    forward();
+    mazeForward();
   }
 
 }
@@ -148,20 +158,16 @@ int main (){
   //{
   //  for (int j = 0; j < 10; ++j)
   //  {
-  //  scan_front = i*50+300; //numbers are smaller further away 
-  //  scan_right = j*50; //front right
-  //  scan_left = 100; //front left
   //  int x=0;
-  mazeSection();
-  //  printf("front: %d left: %d right: %d turnNum: %d\n",scan_front,scan_left,scan_right,turning);
-
+  //mazeSection();
   //  }
   //}
-  /*while (x<loopForceTimer) {
+  while (x<loopForceTimer) {
     mazeSection();
 
     sleep1(0,sleepAmount);
+
     x++;
-  }*/
+  }
 return 0;
 }
