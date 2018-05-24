1433187ffcc51bc5f94fbc17af50c1b7a3915f51
Nicole Stallinger <stallinger.nicole@gmail.com>
2018-05-24 13:44:54 +1200

    testing

--- red_tape_test.cpp
+++ red_tape_test.cpp
@@ -49,7 +49,7 @@ int redTest(int scaning_row) {
 		int red_total = red_total + pix; //add the output to red_total
 			
      }
-     printf("red_total = %f", red_total);
+     printf("red_total = %d", red_total);
      if( red_total > 1000) { //if significant enough red has been found
          stage = stage + 1; //increment redTape
          redStage(stage); //call red
@@ -60,6 +60,6 @@ int redTest(int scaning_row) {
 int main() {
 	while (true) {
 		redTest(scan_front);
-		sleep(1000);
+		sleep1(0,100000);
 	}
 }
